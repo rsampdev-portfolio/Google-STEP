cde03c63477f82f178b60e6986494e9d5eac1b76
Rodney Sampson II <rsampdev@gmail.com>
2020-05-21 18:08:23 -0400

    Clean up to follow style guide indentation rules

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -34,41 +34,41 @@ import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 
 /**
- *	Servlet that handles comment data.
+ *    Servlet that handles comment data.
  */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
-	private final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    private final DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
 
-	@Override
-  	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		List<Comment> comments = new ArrayList<>();
-		
-		Query query = new Query("Comment").addSort("time", SortDirection.DESCENDING);
-		PreparedQuery results = datastore.prepare(query);
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        List<Comment> comments = new ArrayList<>();
+        
+        Query query = new Query("Comment").addSort("time", SortDirection.DESCENDING);
+        PreparedQuery results = datastore.prepare(query);
 
-		for (Entity entity : results.asIterable()) {
-			long id = entity.getKey().getId();
-      		String name = (String) entity.getProperty("name");
-      		String text = (String) entity.getProperty("text");
-			Instant time = Instant.parse((String) entity.getProperty("time"));
+        for (Entity entity : results.asIterable()) {
+            long id = entity.getKey().getId();
+            String name = (String) entity.getProperty("name");
+            String text = (String) entity.getProperty("text");
+            Instant time = Instant.parse((String) entity.getProperty("time"));
 
-			Comment comment = new Comment(id, name, text, time);
-			comments.add(comment);
-		}
+            Comment comment = new Comment(id, name, text, time);
+            comments.add(comment);
+        }
 
-		String json = convertToJSON(comments);
+        String json = convertToJSON(comments);
 
-    	response.setContentType("application/json;");
-    	response.getWriter().println(json);
-  	}
+        response.setContentType("application/json;");
+        response.getWriter().println(json);
+      }
 
-	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		String name = request.getParameter("comment-name");
-		String text = request.getParameter("comment-text");
-		Instant time = Instant.now();
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String name = request.getParameter("comment-name");
+        String text = request.getParameter("comment-text");
+        Instant time = Instant.now();
 
         Entity commentEntity = new Entity("Comment");
         commentEntity.setProperty("name", name);
@@ -77,14 +77,14 @@ public class DataServlet extends HttpServlet {
 
         datastore.put(commentEntity);
 
-		response.sendRedirect("/index.html");
-	}
+        response.sendRedirect("/index.html");
+    }
 
     private String convertToJSON(List<Comment> comments) {
         Gson gson = new Gson();
-		Type type = new TypeToken<List<Comment>>(){}.getType();
-    	String json = gson.toJson(comments, type);
-    	return json;
-  	}
+        Type type = new TypeToken<List<Comment>>(){}.getType();
+        String json = gson.toJson(comments, type);
+        return json;
+      }
 
 }
