09a7694af41c4fad3d5cdd2d48c76b5bd5ef1987
Rodney Sampson II <rsampdev@gmail.com>
2020-05-20 23:54:56 -0400

    Update DataServlet.java to use Datastore

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,14 +19,61 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
+    
+	private ArrayList<Comment> comments = new ArrayList<Comment>();
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
-  }
+	// comments.add("Wow! Your portfolio is super cool.");
+	// comments.add("Neat! I have six siblings as well.");
+	// comments.add("Awesome! We go to the same school.");
+
+	@Override
+  	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String json = convertToJson(comments);
+
+    	response.setContentType("application/json;");
+    	response.getWriter().println(json);
+  	}
+
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String name = request.getParameter("comment-name");
+		String text = request.getParameter("comment-text");
+
+        Entity commentEntity = new Entity("Comment");
+        commentEntity.setProperty("name", name);
+        commentEntity.setProperty("text", text);
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.put(commentEntity);
+
+		addComment(name, text);
+
+		response.sendRedirect("/index.html");
+	}
+
+	public void addComment(String name, String text) {
+        int id = 0;
+
+        if (comments.size() > 0) {
+            id = comments.get(0).getID() + 1;
+        }
+
+        Comment comment = new Comment(id, name, text);
+        comments.add(0, comment);
+    }
+
+    private String convertToJson(ArrayList<Comment> comments) {
+        Gson gson = new Gson();
+		Type type = new TypeToken<List<Comment>>(){}.getType();
+    	String json = gson.toJson(comments, type);
+    	return json;
+  	}
+
 }
