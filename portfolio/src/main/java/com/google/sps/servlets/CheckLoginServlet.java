b606db70dbf905fa8debf306a6a37ef103ad4a47
Rodney Sampson II <rsampdev@gmail.com>
2020-05-27 19:08:23 -0400

    Add CheckLoginServlet.java to return the login status of the user

---
+++ portfolio/src/main/java/com/google/sps/servlets/CheckLoginServlet.java
@@ -0,0 +1,41 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+/**
+ *    Servlet that checks if a user is logged in or not.
+ */
+
+@WebServlet("/check-login")
+public class CheckLoginServlet extends HttpServlet {
+
+    private UserService userService = UserServiceFactory.getUserService();
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        if (userService.isUserLoggedIn()) {
+            String urlToRedirectToAfterUserLogsOut = "/";
+            String logoutUrl = userService.createLogoutURL(urlToRedirectToAfterUserLogsOut);
+            String userEmail = userService.getCurrentUser().getEmail();
+
+            response.getWriter().println("<p>Logged In</p>");
+            response.getWriter().println("<p>Email: " + userEmail + "</p>");
+            response.getWriter().println("<p>Logout <a href=\"" + logoutUrl + "\">here</a></p>");
+        } else {
+            String urlToRedirectToAfterUserLogsIn = "/";
+            String loginUrl = userService.createLoginURL(urlToRedirectToAfterUserLogsIn);
+
+            response.getWriter().println("<p>Not Logged In</p>");
+            response.getWriter().println("<p>Login <a href=\"" + loginUrl + "\">here</a></p>");
+        }
+    }
+
+}
